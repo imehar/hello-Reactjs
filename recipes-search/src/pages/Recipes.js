99b5312b432d91fa7933eed16a4c05e674c9f938
Mehar Madnani <41502248+imehar@users.noreply.github.com>
2020-01-25 22:28:33 +0530

    structure with routing to pages

---
+++ recipes-search/src/pages/Recipes.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Recipes extends Component {
+    render() {
+        return (
+            <div>
+                <h4>hello from recipes</h4>
+            </div>
+        )
+    }
+}
---
+++ recipes-search/src/pages/SingleRecipe.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class SingleRecipe extends Component {
+  render() {
+    return (
+      <div>
+        <h4>single recipe</h4>
+      </div>
+    );
+  }
+}
